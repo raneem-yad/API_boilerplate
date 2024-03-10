794d8ea4c1fdef9445e7dc89e8ffa04772ed4eee
raneem-yad <eng.raneem.yad@gmail.com>
2024-03-10 17:05:55 +0100

    display exception on index.html by JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,25 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e => postForm(e));
 
+function processOptions(form) {
+    let optArray = [];
+
+    for (let e of form.entries()) {
+        if (e[0] === "options") {
+            optArray.push(e[1]);
+        }
+    }
+
+    form.delete("options");
+
+    form.append("options", optArray.join());
+
+    return form;
+}
+
 async function postForm(e) {
 
-    const form = new FormData(document.getElementById("checksform"));
-
+    const form = processOptions(new FormData(document.getElementById("checksform")));
     for (let entry of form.entries()){
         //just to test the form entries
         console.log(entry)
@@ -25,6 +40,7 @@ async function postForm(e) {
     if (response.ok) {
         displayErrors(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 
@@ -63,10 +79,25 @@ async function getStatus(e) {
         displayStatus(data.expiry);
         
     }else {
+        displayException(data);
         throw new Error(data.error)
     }
 
 }
+
+function displayException(data) {
+
+    let heading = `<div class="error-heading">An Exception Occurred</div>`;
+
+    results = `<div>The API returned status code ${data.status_code}</div>`;
+    results += `<div>Error number: <strong>${data.error_no}</strong></div>`;
+    results += `<div>Error text: <strong>${data.error}</strong></div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
+}
+
 
 function displayStatus(data){
     let heading = "API Key Status";
